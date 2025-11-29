aa8edc4b6d39e750fa524aef9b7561dfc3e1f4a6
gmpopov <pgriga@mail.ru>
2025-11-29 19:20:42 +0500

    JSoup PARSER bug fix

--- app/src/main/kotlin/Parser/parser_v2.kt
+++ app/src/main/kotlin/Parser/parser_v2.kt
@@ -4,9 +4,11 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
+const val URL_SITE = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
+
 fun main() {
-    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
+    val doc: Document = Jsoup.connect(URL_SITE).get()
     val quotes: Elements = doc.select("div.sc-2aegk7-2.bzpNIu")
 
-    for (quote in quotes) println(quote.text())
+    quotes.forEach { println(it.text())}
 }
